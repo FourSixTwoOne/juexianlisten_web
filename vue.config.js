69e13238b8322878488ac5eacf739672f78ccb73
肆陆贰壹 <3492214172@qq.com>
2025-01-02 16:03:42 +0800

    初始工程

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,9 @@
 const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+module.exports = {
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: '绝弦音乐共享平台',
+      template: './public/index.html'
+    })
+  ]
+};
